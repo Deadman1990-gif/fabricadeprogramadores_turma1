a59545e36caf9a295bd761de6a7cd7f5a3e0fae7
Deadman1990-gif <pedrohenriquebrandaodecarvalho@gmail.com>
2025-08-05 11:14:09 -0300

    Add files via upload

---
+++ Listas 2.py
@@ -0,0 +1,14 @@
+Lutadores_WWE =["Undertaker", "Kane", "The Rock", "John Cena", "Hulk Hogan", "André The Giant", "Chris Jericho", "The Miz", "Macho Man Randy Savage", "Jake The Snake Roberts"]
+print(Lutadores_WWE[0])
+print(Lutadores_WWE[1])
+print(Lutadores_WWE[2])
+print(Lutadores_WWE[-5])
+Lutadores_WWE.insert(0, "The Rock")
+print(Lutadores_WWE)
+print("----------x--------")
+Lutadores_WWE.pop(0)
+print(Lutadores_WWE)
+print("------x------")
+import random
+cesta=["maçã","banana", "Uva", "limao"]
+print(random.choice(cesta))
---
+++ cofre.py
@@ -0,0 +1,11 @@
+import senha, random, time, turtle
+print(senha.minha_senha)
+print("-------x------")
+numero_aleatorio=random.random()
+print(numero_aleatorio)
+numero_aleatorio=random.randint(0,123456778)
+print(numero_aleatorio)
+print(time.asctime())
+turtle.circle(50)
+turtle.getscreen()._root.mainloop()
+import this
